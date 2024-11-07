9651029ec1880dd3df410b49ab9a6df3004d6357
david3010M <hvaldiviezos@unprg.edu.pe>
2024-11-06 19:34:45 -0500

    swagger

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -12,9 +12,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
  *     description="News resource",
  *     @OA\Property (property="id", type="integer", example="1"),
  *     @OA\Property (property="title", type="string", example="Title"),
+ *     @OA\Property (property="titleEn", type="string", example="Title"),
  *     @OA\Property (property="date", type="string", format="date", example="2024-08-19"),
  *     @OA\Property (property="introduction", type="string", example="Introduction"),
+ *     @OA\Property (property="introductionEn", type="string", example="Introduction"),
  *     @OA\Property (property="description", type="string", example="Description"),
+ *     @OA\Property (property="descriptionEn", type="string", example="Description"),
  *     @OA\Property (property="image", type="string", example="Image"),
  *     @OA\Property (property="active", type="boolean", example="1"),
  *     @OA\Property (property="category_id", type="integer", example="1")
